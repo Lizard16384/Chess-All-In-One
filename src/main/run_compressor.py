16389c5ce7b751c57e9a8f6d6cf312d9d2d365d7
Ben Gianos <77897972+Lizard16384@users.noreply.github.com>
2026-07-15 10:58:04 -0700

    Update run_compressor.py
    Added error cases and improved usability of main structure

--- src/main/run_compressor.py
+++ src/main/run_compressor.py
@@ -18,6 +18,18 @@ spec.loader.exec_module(compress)
 
 import themes
 
+class CommandLengthError(Exception):
+    def __init__(self, value, message):
+        self.value = value
+        self.message = message
+        super().__init__(f"{self.message} (Length: {self.value})")
+
+class CommandWireError(Exception):
+    def __init__(self, value, message):
+        self.value = value
+        self.message = message
+        super().__init__(f"{self.message} (Length: {self.value})")
+
 def read_file_lines(file_name):
     file = open(file_name)
     file_lines = []
@@ -28,32 +40,32 @@ def read_file_lines(file_name):
     file.close()
     return file_lines
 
-def main():
-    raw_command = parse.parse_command(read_file_lines("chess.mcfunction"),read_file_lines("chess_positions.txt"),themes.get_theme_commands())
-
-    snbt= compress.compress_data(raw_command)
-    raw_data = {"snbt":snbt,"storage":"c","scoreboard":"c"}
-
-    final_command = parse.parse_command(read_file_lines("../compressor.mcfunction"),read_file_lines("../positions.txt"),raw_data)
-    if len(final_command) > 32500:
-        print(f"Payload of {len(final_command)} exceeds 32500! Pasting into a command block directly will not be possible!")
+def finish(final, output, result = "result.txt"):
+    if len(final) > 32500:
+        raise CommandLengthError(len(final), f"Payload character length exceeds 32500! Command cannot be pasted in one command!")
+    elif False:  # TODO: check byte length not to exceed 65536 and check behavior - maybe command can still be run, packet just can't be sent back to client.
+        raise CommandWireError(len(final), f"Payload byte length exceeds 65536! Command cannot be sent to server in one command!")
     else:
-        print(f"Payload: {((len(final_command)/32500) * 100):.2f}%, {len(final_command)} used of 32500")
-    result = open("result.txt", "w", -1, "utf-8")
-    result.write(final_command)
-    result.close()
+        print(f"Payload: {((len(final)/32500) * 100):.2f}%, {len(final)} used of 32500")
+    
+    if "clipboard" in output:
+        pyperclip.copy(final)
+    if "write" in output:
+        result = open(result, "w", -1, "utf-8")
+        result.write(final)
+        result.close()
+    if "clipboard" not in output and "write" not in output:
+        raise Exception('Please specify output method. Options are "clipboard" and/or "write"')
 
-    pyperclip.copy(final_command)
+    print(f"Result {f'wrote to file {result}' if "write" in output else ""}{" and " if "clipboard" in output and "write" in output else ""}{"copied to clipboard" if "clipboard" in output else ""}.")
 
-    print("Result wrote to file result.txt and copied to clipboard.")
-
-def test():
+def main():
     raw_command = parse.parse_command(read_file_lines("chess.mcfunction"),read_file_lines("chess_positions.txt"),themes.get_theme_commands())
 
     snbt = compress.compress_data(raw_command)
     raw_data = {"snbt":snbt,"storage":"c","scoreboard":"c"}
 
-    print(raw_data["snbt"])
+    final_command = parse.parse_command(read_file_lines("../compressor.mcfunction"),read_file_lines("../positions.txt"),raw_data)
+    finish(final_command, ("clipboard","write"), "result.txt")
 
 main()
-#test()
